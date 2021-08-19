307f33fd5603eb6f3503c2d96451dd34bf064d10
kameoman <spiderman.935963@gmail.com>
2021-08-19 14:25:30 +0900

    Reducers

---
+++ src/redux/actions/types.js
@@ -0,0 +1,17 @@
+// 数字がクリックされたとき
+export const INPUT_NUMBER = 'INPUT_NUMBER';
+
+// +-*/がクリックされたとき
+export const PLUS = 'PLUS';
+
+export const MINUS = 'MINUS';
+
+export const MULTIPLY = 'MULTIPLY';
+
+export const DIVIDE = 'DIVIDE';
+
+//＝がクリックされたとき。
+export const EQUAL = 'EQUAL';
+
+// ACがクリックされたとき。
+export const CLEAR = 'CLEAR';
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import caluculator from './caluculator';
+
+const reducer = combineReducers({ caluculator });
+
+export default reducer;
